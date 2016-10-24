be63ee1f8120d539982a245def16e354ddbbaa35
srihari sundar <sriharisundar95@gmail.com>
2016-10-24 19:49:38 +0530

    test inverse updated

--- testInverse.cc
+++ testInverse.cc
@@ -3,23 +3,24 @@
 using namespace std;
 
 int main(){
-
-	double **a = new double*[3];
-	for(int i=0;i<3;i++)
-		a[i]=new double[3];
+	
+	double det;
+	double a[6][6]={0};
 	
 	a[0][0]=5;
 	a[1][1]=5;
 	a[2][2]=5;
+	a[3][3]=5;
+	a[4][4]=5;
+	a[5][5]=5;
 
-	minv(a,3);
+	findInverse((double *)a,det,6);
 
-	for(int i=0;i<3;i++){
-		for(int j=0;j<3;j++)
+	for(int i=0;i<6;i++){
+		for(int j=0;j<6;j++)
 			cout<<a[i][j]<<" ";
 		cout<<endl;
 	}
-
 
 	return 0;
 }
